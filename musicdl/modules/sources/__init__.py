941631a4f485dc7f4f614fc59d57298058975d1e
CharlesPikachu <1159254961@qq.com>
2025-12-09 15:30:22 +0800

    kuwo done

--- musicdl/modules/sources/__init__.py
+++ musicdl/modules/sources/__init__.py
@@ -4,15 +4,15 @@ from .mitu import MituMusicClient
 from .joox import JooxMusicClient
 from .base import BaseMusicClient
 from .kuwo import KuwoMusicClient
-# from .migu import MiguMusicClient
+from .migu import MiguMusicClient
 # from .tidal import TIDALMusicClient
 from .lizhi import LizhiMusicClient
 # from .apple import AppleMusicClient
 from .kugou import KugouMusicClient
 from .buguyy import BuguyyMusicClient
 from ..utils import BaseModuleBuilder
-# from .netease import NeteaseMusicClient
-# from .youtube import YouTubeMusicClient
+from .netease import NeteaseMusicClient
+from .youtube import YouTubeMusicClient
 from .gequbao import GequbaoMusicClient
 from .mp3juice import MP3JuiceMusicClient
 from .fivesing import FiveSingMusicClient
@@ -27,7 +27,7 @@ class MusicClientBuilder(BaseModuleBuilder):
         'QQMusicClient': QQMusicClient, 'MituMusicClient': MituMusicClient, 'BuguyyMusicClient': BuguyyMusicClient, 'GequbaoMusicClient': GequbaoMusicClient,
         'MP3JuiceMusicClient': MP3JuiceMusicClient, 'YinyuedaoMusicClient': YinyuedaoMusicClient, 'LizhiMusicClient': LizhiMusicClient, 'XimalayaMusicClient': XimalayaMusicClient,
         'JooxMusicClient': JooxMusicClient, 'KuwoMusicClient': KuwoMusicClient, 'KugouMusicClient': KugouMusicClient, 'FiveSingMusicClient': FiveSingMusicClient,
-        'QianqianMusicClient': QianqianMusicClient,
+        'QianqianMusicClient': QianqianMusicClient, 'MiguMusicClient': MiguMusicClient, 'NeteaseMusicClient': NeteaseMusicClient, 'YouTubeMusicClient': YouTubeMusicClient,
     }
 
 
